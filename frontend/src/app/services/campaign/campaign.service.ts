88ac55872d293c19e87d6ba5659daabe28a9fdef
FilipPietryga <filippatrykpietryga@gmail.com>
2025-03-07 13:43:40 +0100

    Updated the CampaignService in the Frontend to utilize the whole CRUD

--- frontend/src/app/services/campaign/campaign.service.ts
+++ frontend/src/app/services/campaign/campaign.service.ts
@@ -7,12 +7,27 @@ import { Campaign } from '../../models/campaign/campaign.model';
   providedIn: 'root'
 })
 export class CampaignService {
-  private apiUrl = 'http://localhost:8080/api/';
-  private campaignEndpoint = "campaign";
+  private campaignApiUrl = 'http://localhost:8080/api/campaign';
 
   constructor(private http: HttpClient) {}
 
   getAllCampaigns(): Observable<Campaign[]> {
-    return this.http.get<Campaign[]>(this.apiUrl + this.campaignEndpoint);
+    return this.http.get<Campaign[]>(this.campaignApiUrl);
+  }
+
+  getCampaignById(campaignId: number): Observable<Campaign> {
+    return this.http.get<Campaign>(`${this.campaignApiUrl}/${campaignId}`);
+  }
+
+  createCampaign(campaign: Campaign): Observable<Campaign> {
+    return this.http.post<Campaign>(this.campaignApiUrl, campaign);
+  }
+
+  updateCampaign(campaignId: number, campaign: Campaign): Observable<Campaign> {
+    return this.http.put<Campaign>(`${this.campaignApiUrl}/${campaignId}`, campaign);
+  }
+
+  deleteCampaign(campaignId: number): Observable<boolean> {
+    return this.http.delete<boolean>(`${this.campaignApiUrl}/${campaignId}`);
   }
 }
